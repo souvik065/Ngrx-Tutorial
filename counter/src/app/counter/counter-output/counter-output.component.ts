2a89761f1b9eb0eb625ffa8982fba92d2cacf585
souvik <bhattacharyasouvik065@gmail.com>
2023-06-02 17:05:01 +0530

    Using CresteSelector

--- counter/src/app/counter/counter-output/counter-output.component.ts
+++ counter/src/app/counter/counter-output/counter-output.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
 import { CounterState } from '../state/counter.state';
 import { Observable, Subscription } from 'rxjs';
+import { getCounter } from '../state/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -9,16 +10,12 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./counter-output.component.scss']
 })
 export class CounterOutputComponent implements OnInit{
-  counter:number;
+  counter$:Observable<number>;
   constructor(private store: Store<{counter: CounterState}>) {}
 
   ngOnInit(): void{
    
-    this.store.select('counter').subscribe((data)=>{
-      console.log("Counter Observable Called");
-
-      this.counter = data.counter
-    });
+   this.counter$ = this.store.select(getCounter)
    
   }
 

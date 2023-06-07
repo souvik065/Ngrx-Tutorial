ba770fa4943f0192ce3d05a73f4fb4af89146cef
souvik <bhattacharyasouvik065@gmail.com>
2023-06-07 14:16:36 +0530

    Implement Lazyloading for the routes in the ngrx application.

--- counter/src/app/app-routing.module.ts
+++ counter/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { count } from 'rxjs';
+import { CounterComponent } from './counter/counter/counter.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { AddPostComponent } from './posts/add-post/add-post.component';
 import { EditPostComponent } from './posts/edit-post/edit-post.component';
@@ -12,12 +14,15 @@ const routes: Routes = [
   },
   {
     path:'counter',
-    loadChildren:()=> import('./counter/counter.module').then(m=>m.CounterModule),
+    component:CounterComponent
   },
   {
     path:'posts',
-    loadChildren:()=> import('./posts/post.module').then(m=>m.PostModule)
-    
+    component:PostsListComponent,
+    children:[
+      {path:'add', component:AddPostComponent},
+      {path:'edit/:id', component:EditPostComponent}
+    ]
   }
 ];
 
--- counter/src/app/app.module.ts
+++ counter/src/app/app.module.ts
@@ -5,7 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import {StoreModule} from '@ngrx/store';
+import { counterReducer } from './counter/state/counter.reducer';
+import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -16,7 +21,10 @@ import { EditPostComponent } from './posts/edit-post/edit-post.component';
 @NgModule({
   declarations: [
     AppComponent,
-    
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent,
+    CustomCounterInputComponent,
     HomeComponent,
     HeaderComponent,
     PostsListComponent,
